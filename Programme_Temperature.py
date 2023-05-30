eb632b16cc64cbc6b71348e49e6387226af3980c
ARKANGEL OMEGA <guillaume.boissinot@outlook.fr>
2023-05-30 17:00:40 +0200

    FIN

---
+++ Programme_Temperature.py
@@ -0,0 +1,4 @@
+print("Entrez la température svp (F°) :")
+Température_F = input()
+Température_C = (int(Température_F)-int(32)) * (5/9)
+print("Vous avez indiqué " + Température_F + " °F . Cela correspond à " + str(Température_C) + " °C . BRAVO !")
